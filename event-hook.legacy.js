143d6da1fe87f25edb4abeed6259cfe3b6d17ced
muggy8 <muggy8@gmail.com>
2017-05-28 18:17:30 -0700

    udpated legacy mode

--- event-hook.legacy.js
+++ event-hook.legacy.js
@@ -1,33 +1,33 @@
 (function(context){
-	
+
 	/*var defaultConfig = {
-		
+
 	}*/
-	var EventHook = context.EventHook = function(/*config*/){
+	var EventHook = context.EventHook = context.EventHook || function(/*config*/){
 		/*config = config || {};*/
 		var self = this;
 		/*var instanceConfigs = {};
-		
+
 		Object.keys(defaultConfig).forEach(function(key, keyIndex, keyCollection){
 			instanceConfigs[key] = defaultConfig[key]
 		})
 		Object.keys(config).forEach(function(key, keyIndex, keyCollection){
 			instanceConfigs[key] = config[key];
 		})*/
-		
-		var subscription = {}; 
+
+		var subscription = {};
 		/* example
 		{
 			changeHalth:[callback1, callback2, callback3],
 			changeTarget:[callback1, callback2]
 		}
 		*/
-		
+
 		self.subscribe = self.on = function(nameSpace, callback){
 			subscription[nameSpace] = subscription[nameSpace] || [];
 			subscription[nameSpace].push(callback);
 		}
-		
+
 		self.broadCast = self.emit = function(nameSpace, data){
 			data = data || {}
 			if (subscription[nameSpace]){
@@ -40,8 +40,9 @@
 				})*/
 			}
 		}
-		
+
 		self.unsubscribe = self.off = function(nameSpace, callback){
+			if (!subscription[nameSpace]) return
 			for (var i = 0; i < subscription[nameSpace].length; i++){
 				if (subscription[nameSpace][i] == callback){
 					subscription[nameSpace].splice(i, 1);
@@ -55,12 +56,12 @@
 			})*/
 		}
 	}
-	
+
 	/*EventHook.setConfigs = function(config){
 		Object.keys(config).forEach(function(key, keyIndex, keyCollection){
 			defaultConfig[key] = config[key];
 		})
 	}*/
-	
+
 	if (typeof module != 'undefined' ) {module.exports = context.EventHook};
 })(this)
